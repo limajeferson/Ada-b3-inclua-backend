616b19f37d00e19eae7e1f42ad15771897eb64bf
Jeferson Lima <jefersonlima@outlook.com>
2024-08-01 00:33:37 -0300

    Update TesteLivro.java

--- 20240731/b3-poo/src/TesteLivro.java
+++ 20240731/b3-poo/src/TesteLivro.java
@@ -6,6 +6,20 @@ public class TesteLivro {
         livro.anoPublicacao = 1899;
 
         System.out.println(livro.titulo + " é um romance escrito por " + livro.autor + ", publicado em " + livro.anoPublicacao + ".");
+
+        Livro livro2 = new Livro();
+        livro2.titulo = "Vidas Secas";
+        livro2.autor = "Graciliano Ramos";
+        livro2.anoPublicacao = 1938;
+
+        System.out.println(livro2.titulo + " é um romance escrito por " + livro2.autor + ", publicado em " + livro2.anoPublicacao + ".");
+
+        Livro livro3 = new Livro();
+        livro3.titulo = "Capitães da Areia";
+        livro3.autor = "Jorge Amado";
+        livro3.anoPublicacao = 1937;
+
+        System.out.println(livro3.titulo + " é um romance escrito por " + livro3.autor + ", publicado em " + livro3.anoPublicacao + ".");
     }
 
 }
