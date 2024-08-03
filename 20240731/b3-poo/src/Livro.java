2d56d3693fd9caf522f850f322b265432bb83e13
Jeferson Lima <jefersonlima@outlook.com>
2024-08-02 21:55:37 -0300

    Update-aula2-poo

--- 20240731/b3-poo/src/Livro.java
+++ 20240731/b3-poo/src/Livro.java
@@ -1,5 +1,29 @@
 public class Livro {
-    String titulo;
-    String autor;
-    short anoPublicacao;
+    String titulo = "Título não informado";
+    String autor = "Autor não informado";
+    int anoPublicacao = 0;
+
+    public Livro() {
+        this.titulo = "Título não informado";
+        this.autor = "Autor não informado";
+        this.anoPublicacao = 0;
+
+        System.out.println(titulo + " é um romance escrito por " + autor + ", publicado em " + anoPublicacao + ".");
+    }
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        anoPublicacao = 0;
+
+        System.out.println(titulo + " é um romance escrito por " + autor + ", publicado em " + anoPublicacao + ".");
+    }
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+
+        System.out.println(titulo + " é um romance escrito por " + autor + ", publicado em " + anoPublicacao + ".");
+    }
 }
--- 20240731/b3-poo/src/TesteLivro.java
+++ 20240731/b3-poo/src/TesteLivro.java
@@ -1,25 +1,14 @@
 public class TesteLivro {
+
     public static void main(String[] args) {
         Livro livro = new Livro();
         livro.titulo = "Dom Casmurro";
         livro.autor = "Machado de Assis";
         livro.anoPublicacao = 1899;
 
-        System.out.println(livro.titulo + " é um romance escrito por " + livro.autor + ", publicado em " + livro.anoPublicacao + ".");
+        Livro livro2 = new Livro("Vidas Secas","Graciliano Ramos");
 
-        Livro livro2 = new Livro();
-        livro2.titulo = "Vidas Secas";
-        livro2.autor = "Graciliano Ramos";
-        livro2.anoPublicacao = 1938;
+        Livro livro3 = new Livro("Capitães da Areia", "Jorge Amado",1937);
 
-        System.out.println(livro2.titulo + " é um romance escrito por " + livro2.autor + ", publicado em " + livro2.anoPublicacao + ".");
-
-        Livro livro3 = new Livro();
-        livro3.titulo = "Capitães da Areia";
-        livro3.autor = "Jorge Amado";
-        livro3.anoPublicacao = 1937;
-
-        System.out.println(livro3.titulo + " é um romance escrito por " + livro3.autor + ", publicado em " + livro3.anoPublicacao + ".");
     }
-
 }
